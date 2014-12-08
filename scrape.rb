42b772af99689b0b80eb43832f2a34e8afc2ab49
Steinunn <steinunn.olason@gmail.com>
2014-12-08 16:05:45 -0500

    Make into function for ease.

--- scrape.rb
+++ scrape.rb
@@ -5,25 +5,30 @@ require "nokogiri"
 require "open-uri"
 require "date"
 
-file = File.open("index.html", "r").read
+first_date = "2014-12-9"
 
-page = Nokogiri::HTML(file)
+def scrape_page(date)
+  url = "http://www.blogto.com/events/?date=#{date}"
 
-events = page.css(".events-list .events-item")
-date = page.css(".events-overview .info")
-	.text
-	.split(",")[1..2]
-	.join(",")
-
-parsed_events = events.map do |event|
-  {
-	title: event.css(".event-name").text,
-	summary: event.css(".event-summary").text,
-	address: event.css(".event-address").text,
-	date: Date.parse(date),
-	time: event.css(".event-time").text.strip
-  }
+  page = Nokogiri::HTML(open(url))
+  
+  events = page.css(".events-list .events-item")
+  date = page.css(".events-overview .info")
+  	.text
+  	.split(",")[1..2]
+  	.join(",")
+  
+  events.map do |event|
+    {
+  	title: event.css(".event-name").text,
+  	summary: event.css(".event-summary").text,
+  	address: event.css(".event-address").text,
+  	date: Date.parse(date),
+  	time: event.css(".event-time").text.strip
+    }
+  end
 end
 
+results = scrape_page(first_date)
 
 binding.pry
